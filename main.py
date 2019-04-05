c658d8d12d440c615297617fb552a4341881f9a8
Christophe Vauclair <cfvauclair@gmail.com>
2019-04-05 12:48:01 +0800

    Small bug fix

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ num_epochs = 256
 # def random_model():
 #     return NeuralNet(input_size, random.randint(output_size, input_size), output_size)
 
+# Training function definition
 def training_function(model, dataset):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=int(batch_size))
 
@@ -33,6 +34,7 @@ def training_function(model, dataset):
 
     return model
 
+# Testing function definition
 def testing_function(model, dataset):
     # Testing
     with torch.no_grad():
@@ -51,7 +53,8 @@ def testing_function(model, dataset):
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("CUDA is available: {}".format(torch.cuda.is_available()))
-print("Using device: {}".format(torch.cuda.get_device_name(torch.cuda.current_device())))
+if torch.cuda.is_available():
+	print("Using device: {}".format(torch.cuda.get_device_name(torch.cuda.current_device())))
 
 # Load data
 data = pd.read_csv("data/processed.csv", index_col=0)
@@ -91,10 +94,8 @@ w = torch.Tensor([y_data['win'].sum(), y_data['tie'].sum(), y_data['loss'].sum()
 w = w/len(y_data.index)
 criterion = torch.nn.CrossEntropyLoss(weight=w.to(device))
 
-n = 8
-
 start_time = datetime.now()
 loss = validation.holdout_validation(model=model, dataset=dataset, testing_ratio=0.1, training_function=training_function, testing_function=testing_function)
 end_time = datetime.now()
 
-print("Model (1HL size {}) accuracy: {:.4f} % ({})".format(n, 100 * (1.0-loss), end_time - start_time))
+print("Model (1HL size {}) accuracy: {:.4f} % ({})".format(hidden_size, 100 * (1.0-loss), end_time - start_time))
